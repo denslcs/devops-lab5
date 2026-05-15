c6aa2afedc93617bdb9cea2c708c52a88358fc39
denslcs <prikazchikov.denis21@gmail.com>
2026-05-15 22:03:12 +0300

    Add tests and CI workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,32 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': "test@mail.com"})
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User not found"
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json={
+        "name": "Test User",
+        "email": "test@mail.com",
+    })
+    assert response.status_code == 201
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={
+        "name": "Test User",
+        "email": "i.i.ivanov@mail.com",
+    })
+    assert response.status_code == 409
+    assert response.json()["detail"] == "User with this email already exists"
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    client.post("/api/v1/user", json={
+        "name": "Test User",
+        "email": "test@mail.com",
+    })
+    response = client.delete("/api/v1/user", params={'email': "test@mail.com"})
+    assert response.status_code == 204
